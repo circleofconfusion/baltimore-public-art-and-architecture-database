de51d2ff13a48a5a04d2d494a90037d902866029
Shane Knudsen <shaneknu@gmail.com>
2020-10-05 22:00:54 -0400

    Basic partial schema

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,10 @@
-console.log('ran')
+import { ApolloServer } from 'apollo-server';
+import { SCHEMA } from './schema.js';
+
+const server = new ApolloServer({
+  typeDefs: SCHEMA
+});
+
+server.listen().then(({url}) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
--- src/server/schema.js
+++ src/server/schema.js
@@ -8,21 +8,52 @@ export const SCHEMA = gql`
     lastName: String
     email: String
     imageUrl: String
-    birthDate: Date
-    deathDate: Date
+    birthDate: String
+    deathDate: String
+    updated: String
+    updatedBy: ID!
   }
 
   type User implements Person {
+    id: ID!
+    firstName: String
+    middleName: String
+    lastName: String
+    email: String
+    imageUrl: String
+    birthDate: String
+    deathDate: String
+    updated: String
+    updatedBy: ID!
     username: String!
   }
 
   type Artist implements Person {
+    id: ID!
+    firstName: String
+    middleName: String
+    lastName: String
+    email: String
+    imageUrl: String
+    birthDate: String
+    deathDate: String
+    updated: String
+    updatedBy: ID!
     bio: String
-    statement: String
+    website: String
   }
 
   type Artwork {
     id: ID!
+    # artists: [Artist]
+  }
+
+  type Query {
+    users: [User]!
+    user(id: ID!): User
     artists: [Artist]!
+    artist(id: ID!): Artist
+    artworks: [Artwork]!
+    artwork(id: ID!): Artwork
   }
 `;
